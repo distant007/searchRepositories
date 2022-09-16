517ffc481a62cc5e4a836d42d7d48f94ba332f2c
distant007 <74708427+distant007@users.noreply.github.com>
2022-09-16 12:57:40 +0300

    test

--- search.js
+++ search.js
@@ -1,107 +1,86 @@
-class View {
-  constructor() {
-    this.search = document.getElementById("search");
-    this.searchBox = this.createElement("div", "search-input");
-    this.searchInput = this.createElement("input", "input");
-    this.searchInput.placeholder = "Search repositories..";
+const autocomplitBox = document.querySelector(".autocomplit-box");
+const searchInput = document.querySelector(".search-input");
 
-    this.autocomplitBox = this.createElement("div", "autocomplit-box");
+const search = document.getElementById("search");
+const choosedList = document.querySelector(".choosed-list");
 
-    this.search.append(this.searchBox);
-    this.searchBox.append(this.searchInput);
-    this.searchBox.append(this.autocomplitBox);
+const input = document.querySelector("input");
 
-    this.choosed = document.getElementById("choosed");
-    this.itemLi;
+input.oninput = debounce(searchRepo, 200);
 
-    this.autocomplitBox.addEventListener(
-      "click",
-      this.closeAutocomplit.bind(this)
-    );
-  }
-  closeAutocomplit() {
-    this.autocomplitBox.innerHTML = " ";
-    this.searchInput.value = "";
-    this.searchInput.placeholder = "Search repositories..";
-  }
-  createElement(elementTag, elementClass) {
-    const element = document.createElement(elementTag);
-    if (elementClass) element.classList.add(elementClass);
-    return element;
-  }
-  createSearchItem(repoInfo) {
-    const item = this.createElement("li", "item");
-    item.textContent = `${repoInfo.name}`;
+async function searchRepo() {
+  const searchValue = input.value;
 
-    this.autocomplitBox.append(item);
+  if (searchValue) {
+    clearSearch();
 
-    item.dataset.name = `${repoInfo.name}`;
-    item.dataset.owner = `${repoInfo.owner.login}`;
-    item.dataset.owner = `${repoInfo.owner.login}`;
-    item.dataset.stars = `${repoInfo.stargazers_count}`;
-
-    item.addEventListener("click", this.createInfoBlock.bind(item));
-  }
-  createInfoBlock() {
-    const choosedItem = document.createElement("li");
-
-    this.button = document.createElement("button");
-    this.button.classList.add("close-btn");
-
-    choosedItem.innerHTML = `<span>Name: ${this.dataset.name}</span><span>Owner: ${this.dataset.owner}</span><span>Stars: ${this.dataset.stars}`;
-    const choosed = document.getElementById("choosed");
-
-    choosed.append(choosedItem);
-    choosedItem.append(this.button);
-
-    this.button.addEventListener("click", () => {
-      choosedItem.remove();
-    });
-  }
-}
-class Search {
-  constructor(view) {
-    this.view = view;
-
-    this.view.searchInput.addEventListener(
-      "keyup",
-      this.debounce(this.searchRepo.bind(this), 200)
-    );
-  }
-  async searchRepo() {
-    const searchValue = this.view.searchInput.value;
-    if (searchValue) {
-      this.clearSearch();
-
-      await fetch(
-        `https://api.github.com/search/repositories?q=${searchValue}&per_page=5`
-      ).then((response) => {
-        if (response.ok) {
-          response.json().then((responce) => {
-            responce.items.forEach((repo) => {
-              this.view.createSearchItem(repo);
-            });
+    await fetch(
+      `https://api.github.com/search/repositories?q=${searchValue}&per_page=5`
+    ).then((response) => {
+      if (response.ok) {
+        response.json().then((responce) => {
+          responce.items.forEach((repo) => {
+            createSearchItem(repo);
           });
-        }
-      });
-    } else {
-      this.clearSearch();
-    }
-  }
-
-  clearSearch() {
-    this.view.autocomplitBox.innerHTML = " ";
-  }
-  debounce(fn, debounceTime) {
-    let timeout;
-    return function () {
-      const fnCall = () => fn.apply(this, arguments);
-
-      clearTimeout(timeout);
-
-      timeout = setTimeout(fnCall, debounceTime);
-    };
+        });
+      }
+    });
+  } else {
+    clearSearch();
   }
 }
 
-new Search(new View());
+function clearSearch() {
+  autocomplitBox.innerHTML = " ";
+}
+function debounce(fn, debounceTime) {
+  let timeout;
+  return function () {
+    const fnCall = () => fn.apply(this, arguments);
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(fnCall, debounceTime);
+  };
+}
+
+function createElement(elementTag, elementClass) {
+  const element = document.createElement(elementTag);
+  if (elementClass) element.classList.add(elementClass);
+  return element;
+}
+function createSearchItem(repoInfo) {
+  const item = createElement("li", "item");
+  item.textContent = `${repoInfo.name}`;
+
+  autocomplitBox.append(item);
+
+  item.dataset.name = `${repoInfo.name}`;
+  item.dataset.owner = `${repoInfo.owner.login}`;
+  item.dataset.owner = `${repoInfo.owner.login}`;
+  item.dataset.stars = `${repoInfo.stargazers_count}`;
+
+  item.addEventListener("click", () => {
+    createInfoBlock(item);
+    clearSearch();
+    input.value = "";
+  });
+}
+
+function createInfoBlock(item) {
+  const choosedItem = document.createElement("li");
+
+  const button = document.createElement("button");
+  button.classList.add("close-btn");
+
+  choosedItem.innerHTML = `<span>Name: ${item.dataset.name}</span><span>Owner: ${item.dataset.owner}</span><span>Stars: ${item.dataset.stars}`;
+
+  const choosed = document.getElementById("choosed");
+
+  choosed.append(choosedItem);
+  choosedItem.append(button);
+
+  button.addEventListener("click", () => {
+    choosedItem.remove();
+  });
+}
